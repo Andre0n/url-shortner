a66b35451b28f1dde379b7d089fb5e68bdb2c5c3
Andre0n <andreon098@gmail.com>
2024-11-07 22:14:34 -0300

    feat: use `validate_url_request` middleware in main router

--- routes.js
+++ routes.js
@@ -1,13 +1,22 @@
-import { json_body } from './utils/json_body.js';
-import { send_response } from './utils/response.js';
-import { Router } from './utils/router.js';
+import { json_body } from "./utils/json_body.js";
+import { send_response } from "./utils/response.js";
+import { Router } from "./utils/router.js";
+
+import validate_url_request from "./validators/validate_url_request.js";
 
 const router = new Router();
 
 router.use_middleware(json_body);
 
-router.get('/', (_req, res) => {
-  send_response(res, { data: { message: 'Hello World!' } });
+router.get("/", (_req, res) => {
+	send_response(res, { data: { message: "Hello World!" } });
 });
+
+router.put(
+	"/url",
+	validate_url_request((_request, response) => {
+		send_response(response, { data: { message: "URL created" } });
+	}),
+);
 
 export default router;
