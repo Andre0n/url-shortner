439d8e7335c68436ae997f803f2fb6a3857f5e17
Andre0n <andreon098@gmail.com>
2024-12-14 13:07:25 -0300

    feat: add redirect method to UrlController for original URL retrieval

--- modules/url/url.controller.js
+++ modules/url/url.controller.js
@@ -25,4 +25,28 @@ export class UrlController {
 
     return send_response(response, { status: 201, data: shortened_url });
   }
+
+  /**
+   * Redirects to the original URL.
+   *
+   * @param {IncomingMessage} request
+   * @param {ServerResponse} response
+   */
+  static redirect(request, response) {
+    const { short_url } = request.params;
+
+    const original_url = url_service.getOriginalUrl(short_url);
+    if (!original_url) {
+      return send_response(response, {
+        status: 404,
+        data: {
+          error: {
+            message: 'The URL could not be found.',
+          },
+        },
+      });
+    }
+
+    return response.writeHead(301, { Location: original_url }).end();
+  }
 }
