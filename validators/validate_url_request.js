b6d53661e3d85386196a8fc796aba8a88df05eed
Andre0n <andreon098@gmail.com>
2024-12-15 12:59:41 -0300

    feat: enhance URL request validation to include expiration date and visibility checks

--- validators/validate_url_request.js
+++ validators/validate_url_request.js
@@ -13,9 +13,7 @@ const validate_url_request = (handler) => {
    * @returns {void}
    */
   const validate = (request, response) => {
-    // Assumes that the request body is already parsed as JSON
-    const { long_url, tags, expire, custom } = request.body;
-
+    const { long_url, tags, expires, custom, visible } = request.body;
     const erros = [];
     const add_error = (message) => erros.push({ message });
 
@@ -31,9 +29,23 @@ const validate_url_request = (handler) => {
 
     if (custom && typeof custom !== 'string')
       add_error('custom must be a string');
+
     if (tags && !Array.isArray(tags)) add_error('tags must be an array');
-    if (expire && typeof expire !== 'number')
-      add_error('expire must be a number');
+
+    if (expires) {
+      const iso_regex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?Z$/;
+      if (!iso_regex.test(expires))
+        add_error('expires must be a valid ISO date');
+      else if (new Date(expires) < new Date())
+        add_error('expires must be a future date');
+      else request.body.expires = new Date(expires);
+    }
+
+    if (visible) {
+      if (visible !== 'true' && visible !== 'false') {
+        add_error('visible must be a boolean');
+      } else request.body.visible = visible === 'true';
+    }
 
     if (erros.length) {
       return send_response(response, { status: 400, data: { errors: erros } });
