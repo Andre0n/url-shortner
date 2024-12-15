1ed08c0f4bc1c91e1079c8c6592ca63879dbcd11
Andre0n <andreon098@gmail.com>
2024-12-15 16:00:37 -0300

    feat: update migration script to create users table if not exists

--- utils/migrate.js
+++ utils/migrate.js
@@ -2,7 +2,7 @@ import database from './database.js';
 
 database.open();
 database.exec(`
-  CREATE TABLE urls (
+  CREATE TABLE IF NOT EXISTS urls (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     long_url TEXT NOT NULL,
     short_url TEXT NOT NULL,
@@ -14,5 +14,17 @@ database.exec(`
     visible BOOLEAN DEFAULT TRUE,
     UNIQUE (long_url, short_url)
   );
+
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    is_active BOOLEAN DEFAULT TRUE,
+    UNIQUE (username, email)
+  );
+
 `);
 database.close();
