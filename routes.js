f20f8db956245a9cbc5409e2e67ab183887ceff5
Andre0n <andreon098@gmail.com>
2024-12-15 16:02:36 -0300

    feat: add user creation route with request validation

--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
+import { UrlController } from './modules/url/url.controller.js';
+import { UserController } from './modules/user/user.controller.js';
 import { json_body } from './utils/json_body.js';
 import { send_response } from './utils/response.js';
 import { Router } from './utils/router.js';
-import { UrlController } from './modules/url/url.controller.js';
-
+import { validate_user_request } from './validators/user_request.validator.js';
 import validate_url from './validators/validate_url_request.js';
 
 const router = new Router();
@@ -15,5 +16,6 @@ router.get('/', (_req, res) => {
 
 router.put('/url', validate_url(UrlController.shorten));
 router.get('/:short_url', UrlController.redirect);
+router.post('/users', validate_user_request(UserController.create));
 
 export default router;
